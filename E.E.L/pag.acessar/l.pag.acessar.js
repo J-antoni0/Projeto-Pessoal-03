006f9e4eb9e8e1924d1020fc96da7e49acc85d99
João Antônio A. D. S. S. <joao.antonio.amorim.8@gmail.com>
2024-06-28 18:28:05 -0300

    Alterações na Lógica e no HTML
    - Alterado e configurado Lógica.
    - Configurado HTML.

--- E.E.L/pag.acessar/l.pag.acessar.js
+++ E.E.L/pag.acessar/l.pag.acessar.js
@@ -1,18 +1,36 @@
     
-    let texto1 = document.querySelector('input#usuario')
-    let texto2 = document.querySelector('input#senha')
+    let texto1 = document.querySelector('input#usuario');
+    let texto2 = document.querySelector('input#senha');
+    texto1.value = ' ';
+    texto2.value = ' ';
     
     function botaoacessar() {
         
         if (texto1.value == 0 || texto2.value == 0) {
-            window.alert ('Verifique as Credenciais!')
-            texto1.value = ' '
-            texto2.value = ' '
+            window.alert ('Verifique as Credenciais!');
+            texto1.value = '';
+            texto2.value = '';
+            texto1.focus();
+            return;
         }
-        
-        
-    texto1.focus()
-
+        if (texto1.value.length < 6 ) {
+            window.alert ('Usuário deve ter no Minímo 6 Digítos!');
+            texto1.value = '';
+            texto1.focus();
+            return;
+        }
+        if ( texto2.value.length < 6) {
+            window.alert ('Senha deve ter no Minímo 6 Digítos!');
+            texto2.value = '';
+            texto2.focus();
+            return;
+        }else {
+            window.alert ('Acesso Permitido...')
+            texto1.value = '';
+            texto2.value = '';
+            texto1.focus();
+            return;
+        }
     }
 
     
--- E.E.L/pag.se-cadastrar/l.pag.se-cadastrar.js
+++ E.E.L/pag.se-cadastrar/l.pag.se-cadastrar.js
@@ -1,16 +1,44 @@
 
-    let usuario = document.querySelector('input#usuário')
-    let email = document.querySelector('input#e-mail')
-    let senha = document.querySelector('input#senha')
-    let novasenha = document.querySelector('input#novasenha')
+    let usuario = document.querySelector('input#usuário');
+    let email = document.querySelector('input#e-mail');
+    let senha = document.querySelector('input#senha');
+    let novasenha = document.querySelector('input#novasenha');
 
     function botaocadastrar() {
         
         if (usuario.value == 0 || email.value == 0 || senha.value == 0 || novasenha.value == 0) {
-            window.alert ('Certifique-se de que todos os Campos estejam Preenchidos !')
-            usuario.value = ' '
-            email.value = ' '
-            senha.value = ' '
-            novasenha.value = ' '
+            window.alert ('Certifique-se de que todos os Campos Estejam Preenchidos !\n\nNome de Usuário deve conter no minímo 6 Digítos.\nFormato do E-mail deve estar Correto.\nSenha deve conter no Minímo 6 Digítos ("Nova Senha" e "Confirmar Senha" devem estar iguais).');
+            usuario.value = '';
+            email.value = '';
+            senha.value = '';
+            novasenha.value = '';
+            usuario.focus();
+            return;
         }
+        if (usuario.value.length < 6 ) {
+            window.alert ('Usuário deve ter no Mínimo 6 Dígitos ou Mais !');
+            usuario.focus();
+            usuario.value = '';
+            return;
+        }
+        if (!email.value.endsWith('@outlook.com') && !email.value.endsWith('@gmail.com')) {
+            window.alert ('Formato do E-mail está incorreto !');
+            email.focus();
+            email.value = '';
+            return;
+        }
+        if (senha.value.length < 6) {
+            window.alert ('Senha deve Conter no Mínimo 6 Dígitos !');
+            senha.value = '';
+            novasenha.value = '';
+            senha.focus();
+            return;
+        } if (senha.value !== novasenha.value ) {
+            window.alert ('As senhas dígitadas não se Coincidem !');
+            senha.value = '';
+            novasenha.value = '';
+            senha.focus();
+            return;
+        }
+        
     }
